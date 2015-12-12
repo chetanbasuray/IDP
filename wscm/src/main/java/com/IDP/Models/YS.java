ef472b7ea63502624c6dcc61ef8dd6049510b1ce
Saion Chatterjee <saionx2@gmail.com>
2015-12-12 19:42:12 +0100

    New Sources Updated with Maven Integration

---
+++ wscm/src/main/java/com/IDP/Models/YS.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package com.IDP.Models;
+
+import java.util.List;
+
+/**
+ * @author JOY
+ *
+ */
+public class YS {
+	private String units;
+	private List<Integer> timePoints;
+	/**
+	 * @return the units
+	 */
+	public String getUnits() {
+		return units;
+	}
+	/**
+	 * @param units the units to set
+	 */
+	public void setUnits(String units) {
+		this.units = units;
+	}
+	/**
+	 * @return the timePoints
+	 */
+	public List<Integer> getTimePoints() {
+		return timePoints;
+	}
+	/**
+	 * @param timePoints the timePoints to set
+	 */
+	public void setTimePoints(List<Integer> timePoints) {
+		this.timePoints = timePoints;
+	}
+}
